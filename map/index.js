db780c1b77ad2ed09fef9fe244ed83f1e5ac6f4b
ObfuscatedFuture <obfuscatedfuture@protonmail.com>
2022-11-13 21:26:31 -0500

    ...

--- map/index.js
+++ map/index.js
@@ -1,6 +1,18 @@
 var screenWidth = window.innerWidth;
 var screenHeight = window.innerHeight;
 
-function calculateDaysBetweenDates(begin, end) {
-    
-}
+
+//Determine whether the user is on a mobile device
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+//If the user is on a mobile device, set the screen width and height to the size of the screen
+if (isMobile) {
+    screenWidth = screen.width;
+    screenHeight = screen.height;
+    }
+else
+    {
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    }
+
---
+++ volunteer/index.js
@@ -0,0 +1,9 @@
+//create a userName and userPassword field
+
+var userName = document.getElementById("userName");
+var userPassword = document.getElementById("userPassword");
+
+//create an email field
+
+var userEmail = document.getElementById("userEmail");
+
